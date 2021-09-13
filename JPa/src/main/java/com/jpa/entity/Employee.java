f0ce24aea438a81e2884350eb471b66c6a9626ad
leanhthanhtai <leanhthanhtai@gmail.com>
2021-09-13 17:09:19 +0700

    Phan Master

---
+++ JPa/src/main/java/com/jpa/entity/Employee.java
@@ -0,0 +1,57 @@
+package com.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstname;
+	private String lastname;
+	private int age;
+	
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(String firstname, String lastname, int age) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
